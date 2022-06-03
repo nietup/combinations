cc163a5e6c5c444ede31d889507a78322094b915
nietup <22.jakub@gmail.com>
2022-06-03 12:14:17 +0200

    fix state pop, works fine

--- combinations.kt
+++ combinations.kt
@@ -1,26 +1,24 @@
 fun isValidSolution(state: MutableList<Int>, k: Int): Boolean = state.size == k
 
 fun getCandidates(state: MutableList<Int>, n: Int, k: Int): MutableSet<Int> {
-    if (state.size >= k) {
-        return mutableSetOf()
+    return if (state.isEmpty()) {
+        (0 until n).toMutableSet()
+    } else {
+    	((state.last() + 1) until n).toMutableSet()
     }
-
-    return if (state.isEmpty())
-        	(0 until n).toMutableSet()
-    	else
-    		((state.last() + 1) until n).toMutableSet()
 }
 
 fun findCombinations(n: Int, k: Int, state: MutableList<Int>, solutions: MutableSet<MutableList<Int>>) {
     if (isValidSolution(state, k)) {
         solutions.add(state.toMutableList())
+        return
     }
     
     for (i in getCandidates(state, n, k)) {
         state.add(i)
         findCombinations(n, k, state, solutions)
         // todo why
-        state.dropLast(1)
+        state.removeLast()
     }
 }
 
@@ -32,5 +30,5 @@ fun solve(n: Int, k: Int): MutableSet<MutableList<Int>> {
 }
 
 fun main() {
-    print(solve(3, 2))
+    print(solve(5, 3))
 }
