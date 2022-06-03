d5816e2bdb2b50baeff4b04c34e212fedad70fac
nietup <22.jakub@gmail.com>
2022-06-03 09:52:39 +0200

    bugfix - returns one valid solution

--- combinations.kt
+++ combinations.kt
@@ -1,46 +1,36 @@
-fun isValidSolution(state: MutableList<Int>, k: Int): Boolean {
-    if (state.size != k)
-    	return false
-    
-    for (i in 1 until state.size) {
-        if (state[i] <= state[i - 1]) {
-            return false
-        }
-    }
-    
-    return true
-}
+fun isValidSolution(state: MutableList<Int>, k: Int): Boolean = state.size == k
 
 fun getCandidates(state: MutableList<Int>, n: Int, k: Int): MutableSet<Int> {
     if (state.size >= k) {
         return mutableSetOf()
     }
-    
-    //todo candidate should be one number?
+
     return if (state.isEmpty())
         	(0 until n).toMutableSet()
     	else
-    		((state.last()) until n).toMutableSet()
+    		((state.last() + 1) until n).toMutableSet()
 }
 
-fun findCombinations(n: Int, k: Int, state: MutableList<Int>, solutions: MutableSet<MutableList<Int>>): MutableSet<MutableList<Int>> {
+fun findCombinations(n: Int, k: Int, state: MutableList<Int>, solutions: MutableSet<MutableList<Int>>) {
     if (isValidSolution(state, k)) {
         solutions.add(state.toMutableList())
-        return solutions
     }
     
     for (i in getCandidates(state, n, k)) {
         state.add(i)
-        //todo how
         findCombinations(n, k, state, solutions)
         // todo why
         state.dropLast(1)
     }
-    
-    //todo should this be here?
-    return mutableSetOf()
 }
- 
+
+fun solve(n: Int, k: Int): MutableSet<MutableList<Int>> {
+    val state = mutableListOf<Int>()
+    val solutions = mutableSetOf<MutableList<Int>>()
+    findCombinations(n, k, state, solutions)
+    return solutions
+}
+
 fun main() {
-    print(findCombinations(3, 2, mutableListOf(), mutableSetOf()))
+    print(solve(3, 2))
 }
